ecf5bfb0ce7a88b36c59a7a6f43d7e195c521b75
Benoit Dubreuil <lol12lol12lol12lol12@hotmail.com>
2018-12-16 10:31:45 -0500

    Completed and fixed the removeNth code and main

--- GenericProgrammingAndTheSTL/Main.cpp
+++ GenericProgrammingAndTheSTL/Main.cpp
@@ -1,9 +1,21 @@
 #include <iostream>
+#include <iterator>
+#include <vector>
+
 #include "RemoveNth.h"
 
 int main()
 {
+	using ContainerType = std::vector<int>;
+	ContainerType data = {1, 2, 3, 2, 3, 1, 1, 3, 3, 2, 1, 1};
 
+	std::copy(data.begin(), data.end(), std::ostream_iterator<ContainerType::value_type>(std::cout, " "));
+	std::cout << std::endl;
+
+	ContainerType::iterator removedBegin = removeNth(data.begin(), data.end(), 3);
+	data.resize(std::distance(data.begin(), removedBegin));
+
+	std::copy(data.begin(), data.end(), std::ostream_iterator<ContainerType::value_type>(std::cout, " "));
 
 	return 0;
 }
--- GenericProgrammingAndTheSTL/RemoveNth.h
+++ GenericProgrammingAndTheSTL/RemoveNth.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <cassert>
+
 // Example 2-3(d): Solving the problems
 //
 // Precondition:
@@ -8,7 +10,7 @@
 template<typename FwdIter>
 FwdIter removeNth(FwdIter first, FwdIter last, size_t n)
 {
-	assert(distance(first, last) >= n);
+	assert(distance(first, last) >= static_cast<typename std::iterator_traits<FwdIter>::difference_type>(n));
 
 	advance(first, n);
 	if (first != last)
